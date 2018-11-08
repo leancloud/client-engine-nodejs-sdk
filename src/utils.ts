5d51de53b0403bf9c06079f2d91988a6b90f51c3
leeyeh <lee55962698@gmail.com>
2018-11-08 13:04:32 +0800

    fix: convert play error event payload to Error if possible
    This ensure an error will be thrown for all APIs.

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,31 @@
-import { EventEmitter as PlayEventEmitter } from "@leancloud/play";
+import { EventEmitter as PlayEventEmitter, PlayEvent } from "@leancloud/play";
 import { EventEmitter } from "events";
 import generate = require("nanoid/generate");
 
-export function listen<T, K extends keyof T, L extends keyof T>(
+export function listen<T extends PlayEvent, K extends keyof T, L extends keyof T>(
   target: PlayEventEmitter<T>,
   resolveEvent: K,
   rejectEvent?: L,
 ) {
   return new Promise<T[K]>((resolve, reject) => {
-    target.once(resolveEvent, resolve);
+    let rejectCallback: (error: T[L]) => any;
+    const resolveCallback = (payload: T[K]) => {
+      if (rejectEvent) {
+        target.off(rejectEvent, rejectCallback);
+      }
+      resolve(payload);
+    };
+    target.once(resolveEvent, resolveCallback);
     if (rejectEvent) {
-      target.once(rejectEvent, reject);
+      rejectCallback = (error) => {
+        target.off(resolveEvent, resolveCallback);
+        if (error instanceof Error) {
+          reject(error);
+        } else {
+          reject(new Error((error as any).detail || JSON.stringify(error)));
+        }
+      };
+      target.once(rejectEvent, rejectCallback);
     }
   });
 }
